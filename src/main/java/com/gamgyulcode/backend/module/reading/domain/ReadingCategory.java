9abf9d528b5082af9be439f8726b052e28f10475
gywns0417 <gywns0417@gmail.com>
2024-05-23 23:18:46 +0900

    feat: ReadingCategory 개수 줄임

--- src/main/java/com/gamgyulcode/backend/module/reading/domain/ReadingCategory.java
+++ src/main/java/com/gamgyulcode/backend/module/reading/domain/ReadingCategory.java
@@ -1,5 +1,5 @@
 package com.gamgyulcode.backend.module.reading.domain;
 
 public enum ReadingCategory {
-    NAME, ADDRESS, PHONE, OPERATING_HOURS, FEE, TALE, TERRAIN, CAUTION, HISTORY, ORIGINATE, TIP
+    NAME, ADDRESS, PHONE, OPERATING_HOURS, FEE, TALE, TERRAIN, CAUTION, HISTORY
 }
